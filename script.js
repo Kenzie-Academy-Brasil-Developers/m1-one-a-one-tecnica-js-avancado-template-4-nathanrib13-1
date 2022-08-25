fd684f2260db542c9eb9b3b3b4f0f05545a29cfd
Nathan Ribeiro <nathan.ribeiros7@gmail.com>
2022-08-25 12:17:25 -0300

    enviando 1:1

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+function encontrarValor  (arr, num){
+    let  totalEncontrado = 0 
+    for (let i = 0; i<arr.length; i++){
+        if(arr[i] !== num && num > 0){
+            totalEncontrado +=1
+        }
+    }
+return totalEncontrado
+}
+
+let result = encontrarValor([1,1,2,1,1], 1)
+console.log(result)
